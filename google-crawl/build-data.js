7fdefb5d1d9e0f73575f21f951124e3a05a83d55
haibui <hoanghaibui2003@gmail.com>
2018-12-28 14:28:05 +0800

    Create adapter for ifarmer

--- google-crawl/build-data.js
+++ google-crawl/build-data.js
@@ -1,38 +1,32 @@
 const _ = require('lodash');
-const util = require('./lib/util');
 const fs = require('fs');
-var globule = require('globule');
-var path = "parse-csv/data/international-targeting/ph.csv";
-function process() {
-	 let files = globule.find(path);
-	_.each(files, file =>{
-		let fileName = file.split("/");
-		fileName= fileName[fileName.length-1];
-		fs.readFile(file, 'utf8', function (err, data) {
-			var json = util.CSVToArray(data);
-			json = util.buildInternationalArr(json);
-			var lineArray = [];
-			json.forEach(function (infoArray, index) {
-				var line = "";
-				infoArray.forEach((item, i) => {
-					if (i === 3 && index !== 0) {
-						line += ",\"" + item + "\"";
-					} else {
-						line += line ? "," + item : item;
-					}
-				});
-				lineArray.push(line);
-			});
-			var csvContent = lineArray.join("\n");
-			fs.writeFile("results/international-targeting/"+fileName, csvContent, 'utf8', function (err) {
-				if (err) {
-					console.log('Some error occured - file either not saved or corrupted file saved.');
-				} else {
-					console.log('It\'s saved!');
-				}
+const globule = require('globule');
+const product = require("../lib/product");
+const variant = require("../lib/variant");
+const shortDescription = require("../lib/shortDescription");
+const saveFile = require("../lib/saveFile");
+const path = "google/*/*.json";
+function process(){
+	let files = globule.find(path);
+	let products = [];
+	let variants = [];
+	_.each(files, async file => {
+		await fs.readFile(file, 'utf8', function (err, data) {
+			let json = JSON.parse(data);
+			let prod = product.create(json);
+			products.push(prod);
+			let vars = variant.create(prod.id, prod.variants);
+			shortDescription.generateForVariants(vars, prod.name);
+			_.each(vars, v => {
+				variants.push(v);
 			});
 		});
 	});
+	setTimeout(()=>{
+		saveFile.save("ifarmer/products.json", JSON.stringify(products));
+		saveFile.save("ifarmer/variants.json", JSON.stringify(variants));
+	},5000);
+
 
 }
 process();
