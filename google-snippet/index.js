4571c4cee5345efd8c5c1963e91df53f99fe8d13
haibui <hoanghaibui2003@gmail.com>
2019-01-09 21:56:40 +0800

    Inscrese timeout

--- google-snippet/index.js
+++ google-snippet/index.js
@@ -3,23 +3,26 @@ const csv = require("../lib/csv");
 const keywordGen = require("../lib/keyword");
 const crawlGoogleSnippet = require("../lib/crawlGoogleSnippet");
 const fs = require('fs');
-const DELAY = 1000;
+const DELAY = 20000;
 
 function process() {
 	const file = "google-snippet/keywords.csv";
 	fs.readFile(file, 'utf8', function (err, data) {
 		let arr = csv.CSVToArray(data);
 		let offset = 0;
-		arr.map(function (item) {
+		let googleURL = arr[0][0] || "https://www.google.co.id/search?q=";
+		arr.map(function (item, index) {
+			if(index === 0){
+				return;
+			}
 			setTimeout(function () {
 				console.log(item);
 				if (!item[0]) {
 					return;
 				}
 				let keyword = keywordGen.keywordSearch(item[0]);
-				let processedData = [];
-				let googleURL = "https://www.google.co.id/search?q=" + keyword;
-				let deferer = crawlGoogleSnippet.crawl(googleURL).then((data) => {
+				let url = googleURL +keyword;
+				let deferer = crawlGoogleSnippet.crawl(url).then((data) => {
 					saveFile.updateFile("google-snippet/result.csv", item[0] + "," + data.join(", "));
 				});
 
