dec05893608592bed3e6d695260a76bf56ace061
haibui <hoanghaibui2003@gmail.com>
2019-03-04 15:51:44 +0800

    Add article

--- articles/build-data.js
+++ articles/build-data.js
@@ -5,12 +5,15 @@ const doc = require("../lib/doc");
 const saveFile = require("../lib/saveFile");
 const path = "articles/data/*.doc";
 let variant;
+const dir = require('../lib/dir');
 
 function main() {
+	dir.make("articles/result");
 	let files = globule.find(path);
 	_.each(files, async file => {
 		await fs.readFile(file, 'utf16le', function (err, data) {
 				let arr = doc.parseFiles(data);
+			saveFile.save("articles/result/articles.json", JSON.stringify(arr));
 		});
 	})
 }
