95e43abce2341ebe456b9e3ae40f1901cb295504
haibui <hoanghaibui2003@gmail.com>
2019-03-05 22:44:34 +0800

    Tool catagorize page type

---
+++ iprice/categorize-link.js
@@ -0,0 +1,65 @@
+const _ = require('lodash');
+const globule = require('globule');
+const saveFile = require("../lib/saveFile");
+const csv = require("../lib/csv");
+const pageTypeFile = "./iprice/condition/page-type.csv";
+const path = "./iprice/data/*.csv";
+
+async function main() {
+	let pageTypes = await csv.getArrDataFromCSV(pageTypeFile);
+	console.log(pageTypes.length);
+	let files = globule.find(path);
+	for (let i = 0; i < files.length; i++) {
+		let file = files[i];
+		let data = await csv.getArrDataFromCSV(file);
+		let processedArr = categorizePageType(data, pageTypes);
+		saveFile.saveCSVFile(file.replace("data", "result"), processedArr);
+	}
+}
+
+function categorizePageType(arr, pageTypes) {
+	let result = [];
+	for (let i = 0; i < arr.length; i++) {
+		let item = arr[i];
+		let data = _.cloneDeep(item);
+		let url = data[5];
+		data[14] = getTypePage(url, pageTypes);
+		result.push(data);
+	}
+	return result;
+}
+
+function getTypePage(str, arr) {
+	let result = "PLP";
+	_.each(arr, item => {
+		if (isMatchPattern(str, item[1], item[2])) {
+			result = item[3];
+			return false;
+		}
+	});
+	if (result === "Homepage" && !isHomePage(str)) {
+		result = "PLP";
+	}
+	if (result === "Homepage") {
+		// console.log(str, result);
+	}
+	return result;
+}
+
+function isHomePage(str) {
+	return !!str.match(/^http(s)?:\/\/[a-z0-9-]+(\.[a-z0-9-]+)*?(:[0-9]+)?(\/)?$/i);
+}
+
+function isMatchPattern(str, patern1, patern2) {
+	if (!str || !patern2 || !patern1) {
+		return false;
+	}
+	str = str.toLowerCase();
+	patern1 = patern1.toLowerCase();
+	patern2 = patern2.toLowerCase();
+	var expression = `(${patern1}${patern2})`;
+	var rx = new RegExp(expression, 'g');
+	return !!str.match(rx);
+}
+
+main();
